da7c09135ed7e8275320ec6065e5eb801e9584a3
Jannilsonn <jannilsonn@gmail.com>
2021-05-12 14:29:44 -0300

    Refatorando e testando mensagens de erros

--- lib/file_data.rb
+++ lib/file_data.rb
@@ -10,49 +10,60 @@ class FileData
     return 'ERROR: Only (Array) is accepted as parameter' if invoices.class != Array
     return 'ERROR: (Array) cannot be empty!' if invoices.empty?
 
+    all_errors = errors(invoices)
+    return all_errors unless all_errors.empty?
+
+    file_data = format(invoices)
+
+    fusion(file_data)
+  end
+
+  private
+
+  def self.errors(file_data)
+    all_errors = {}
+    
+    file_data.filter_map do |invoice|
+      file_name = Validate.file_name(pay_type: invoice.pay_type, type: invoice.type)
+      all_errors[:file_name] = file_name if file_name.include? 'ERROR:'
+
+      token = Validate.invoice_token(token: invoice.token)
+      all_errors[:token] = token if token.include? 'ERROR:'
+
+      due_date = Validate.invoice_due_date(due_date: invoice.due_date)
+      all_errors[:due_date] = due_date if due_date.include? 'ERROR:'
+
+      value = Validate.invoice_value(value: invoice.value)
+      all_errors[:value] = value if value.include? 'ERROR:'
+
+      status = Validate.invoice_status(status: invoice.status)
+      all_errors[:status] = status if status.include? 'ERROR:'
+    end
+    
+    all_errors
+  end
+
+  def self.format(invoices)
     file_data = invoices.map do |invoice|
       new(
         key: "#{Validate.file_name(pay_type: invoice.pay_type, type: invoice.type)}",
         body: [
           "#{Validate.invoice_token(token: invoice.token)}"\
-          "#{Validate.invoice_due_date(expiration_date: invoice.expiration_date)}"\
+          "#{Validate.invoice_due_date(due_date: invoice.due_date)}"\
           "#{Validate.invoice_return_date(invoice.return_date)}"\
           "#{Validate.invoice_value(value: invoice.value)}"\
           "#{Validate.invoice_status(status: invoice.status)}"
         ]
       )
     end
+  end
 
-    all_errors = errors(file_data)
-    return all_errors unless all_errors.empty?
-
+  def self.fusion(file_data)
     file_data.combination(2) do |obj|
       if obj[0].key == obj[1].key
         obj[0].body << obj[1].body.join
         file_data.delete(obj[1])
       end
     end
-  end
-
-  def self.errors(file_data)
-    all_errors = {}
-    
-    file_data.filter_map do |invoice|
-      if invoice.body.join.include?('ERROR:')
-        error_body = invoice.body.join.split('ERROR:')
-        all_errors[:token] = error_body[1] unless error_body[1].empty?
-        all_errors[:expiration_date] = error_body[2] unless error_body[2].empty?
-        all_errors[:value] = error_body[3] unless error_body[3].empty?
-        all_errors[:status] = error_body[4] unless error_body[4].empty?
-      end
-
-      if invoice.key.include?('ERROR:')
-        error_key = invoice.key.split('ERROR:')
-        all_errors[:total] = error_key[1] unless error_key[1].empty?
-        all_errors[:amount] = error_key[2] unless error_key[2].empty?
-      end
-    end
-    
-    all_errors
   end
 end
--- spec/invoices_spec.rb
+++ spec/invoices_spec.rb
@@ -21,8 +21,7 @@ describe 'Gerenciar faturas' do
   end
 
   context 'Gerar arquivos de faturas pagas' do
-    Invoice.create
-    
+    let(:invoices) { Invoice.create }
     it 'Mudar fatura para paga' do
       Invoice.pay
 
@@ -38,6 +37,104 @@ describe 'Gerenciar faturas' do
 
       expect(Dir.new("./invoices/paid").each_child.map  { |file| file }).not_to eq([])
       expect(Dir.new("./invoices/unpaid").each_child.map  { |file| file }).to eq([])
+    end
+  end
+
+  context 'Mensagens de erros do key para nome do arquivo' do
+    let(:error_file_name) { 'ERROR: Validate.file_name expected 2 parameters' }
+    
+    it 'Mensagem de erro para nome do arquivo com FileData.build' do
+      invoice = FileData.build([
+        Invoice.new(type: 'emissao', pay_type: '',
+                    token: '15a48166c4d2121a7ac7', due_date: '2020-04-20',
+                    value: '100.50', status: '1')
+      ])
+
+      expect(invoice[:file_name]).to eq error_file_name
+    end
+  end
+
+  context 'Mensagens de erros do body para token' do
+    let(:error_token) { 'ERROR: Validate.invoice_token expected (1) parameter with (20) chars' }
+    let(:invoice) {
+      Invoice.new(type: 'EMISSAO', pay_type: 'CARTAODECREDITO',
+        token: '', due_date: '2020-04-20',
+        value: '100.50', status: '1')
+    }
+    
+    it 'Mensagem de erro para token com FileData.build' do
+      error = FileData.build([invoice])
+
+      expect(error[:token]).to eq error_token
+    end
+
+    it 'Mensagem de erro para token com Invoice.create' do
+      error = Invoice.create([invoice])
+
+      expect(error[:token]).to eq error_token
+    end
+  end
+
+  context 'Mensagens de erros do body para data de vencimento' do
+    let(:error_due_date) { 'ERROR: Validate.invoice_due_date expected (1) parameter of type date' }
+    let(:invoice) {
+      Invoice.new(type: 'EMISSAO', pay_type: 'CARTAODECREDITO',
+        token: '15a48166c4d2121a7ac7', due_date: '',
+        value: '100.50', status: '1')
+    }
+
+    it 'Mensagem de erro para data de vencimento com FileData.build' do
+      error = FileData.build([invoice])
+
+      expect(error[:due_date]).to eq error_due_date
+    end
+
+    it 'Mensagem de erro para data de vencimento com Invoice.create' do
+      error = Invoice.create([invoice])
+
+      expect(error[:due_date]).to eq error_due_date
+    end
+  end
+
+  context 'Mensagens de erros do body para valor da fatura' do
+    let(:error_value) { 'ERROR: Validate.invoice_value expected (1) parameter' }
+    let(:invoice) {
+      Invoice.new(type: 'EMISSAO', pay_type: 'CARTAODECREDITO',
+        token: '15a48166c4d2121a7ac7', due_date: '2020-04-20',
+        value: '', status: '1')
+    }
+
+    it 'Mensagem de erro para valor da fatura com FileData.build' do
+      error = FileData.build([invoice])
+
+      expect(error[:value]).to eq error_value
+    end
+
+    it 'Mensagem de erro para valor da fatura com Invoice.create' do
+      error = Invoice.create([invoice])
+
+      expect(error[:value]).to eq error_value
+    end
+  end
+
+  context 'Mensagens de erros do body para valor da fatura' do
+    let(:error_status) { 'ERROR: Validate.invoice_status expected (1) parameter' }
+    let(:invoice) {
+      Invoice.new(type: 'EMISSAO', pay_type: 'CARTAODECREDITO',
+        token: '15a48166c4d2121a7ac7', due_date: '2020-04-20',
+        value: '100.50', status: '')
+    }
+
+    it 'Mensagem de erro para valor da fatura com FileData.build' do
+      error = FileData.build([invoice])
+
+      expect(error[:status]).to eq error_status
+    end
+
+    it 'Mensagem de erro para valor da fatura com Invoice.create' do
+      error = Invoice.create([invoice])
+
+      expect(error[:status]).to eq error_status
     end
   end
 end
